48a2ca3c3d79fa1f29403d06d8f9f23d826b277e
Javi <javiercamarerolopez@gmail.com>
2024-01-23 15:01:26 +0100

    Build 23/01/2023

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("hola mundo")
+async function getPkmn(randomPkmn) {
+    const url = `https://pokeapi.co/api/v2/pokemon/${randomPkmn}`;
+    const respuesta = await fetch(url);
+    const datos = await respuesta.json();
+    return datos;
+}
+
+function randomNumber(max){
+    return Math.floor(Math.random() * max) + 1;
+
+}
+
+async function mi_peticion(){
+    const pkmn = await getPkmn(randomNumber(1008));
+    const name = pkmn.name;
+    const img = pkmn.sprites.front_default;
+
+    console.log(name)
+    console.log(img)
+}
+
+mi_peticion();
